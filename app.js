d66d3e34c11bdbfc989c38297aebde1abbb13f8e
Ramakrishnan Iyer <iyerramakrishnan5@gmail.com>
2017-11-25 21:22:45 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs= require('hbs');
+const fs=require('fs');
+var app = express();
+//console.log(__dirname);
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+app.set('view engine','hbs');
+//Creating a middleware
+app.use((req,res,next)=>{
+    var currentDate= new Date().toString();
+    var log=`${currentDate} ${req.method} ${req.url}`;
+    fs.appendFile('log.txt',log+'\n',(err)=>{
+            if(err){
+                console.log('Unable to log');
+            }
+    });
+
+next();
+});
+/*app.use((req,res,next)=>{
+    res.render('maintenence.hbs');
+})*/
+app.use(express.static(__dirname+'/public'));
+//Using get to call the index page
+app.get('/',(request,response)=>{
+    //Use Render to call an hbs view
+    response.render('index.hbs',{
+        currentPage:'Home',
+        welcomeMessage:'Welcome to my site!'
+    });
+});
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        currentPage:'About'
+    });
+});
+app.get('/bad',(req,res)=>{
+    res.send([{
+        errormsg:'Unable to load servers'
+    }]);
+});
+app.listen(3000);
